8064d601ab5a4913abf3380eb8d0a2c5d99b1106
divyanshu <divyanshu.r46956@gmail.com>
2016-10-16 18:23:54 +0530

    html/css implemented for slider git add .

--- js/script.js
+++ js/script.js
@@ -231,11 +231,24 @@ function sort_price(data)
 
 		}
 
-		// console.log(JSON.stringify(modified_obj,null,4));
 		append_data(modified_obj);
 
 
 	}
+
+ $( function() {
+    $( "#slider-range" ).slider({
+      range: true,
+      min: 0,
+      max: 500,
+      values: [ 75, 300 ],
+      slide: function( event, ui ) {
+        $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+      }
+    });
+    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+      " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+  } );
 
 	
 		$("#reload_it").on("click", function(){
